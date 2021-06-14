fae491502d9c28734ec11894e6749851fde89746
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-06-14 17:11:10 +0800

    Finish part1-template

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ function App() {
   return (
     <div className="container">
       <header>
-        <a href="/">kitani</a>
+        <a href="/">
+          <div className="header__logo"></div>
+        </a>
+        <div className="header__titles">
+          <p className="header__titles__title">AAAAA</p>
+          <p className="header__titles__subtitle">aaaaa</p>
+        </div>
       </header>
-      <main>pageeeeees</main>
+      <main>
+        <div className="main__banner"></div>
+        <div>pageeeeees</div>
+      </main>
       <footer>All right is reserved.</footer>
     </div>
   );
