cd3bead9eb0858acc96b06eae41b38bab4fab2cb
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-06-14 15:14:22 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <a href="/">kitani</a>
+      </header>
+      <main>pageeeeees</main>
+      <footer>All right is reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
