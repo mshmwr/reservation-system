bc857e9aa1b4860d336d7febca20aec470cc6575
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-06-15 22:01:22 +0800

    add-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-
-      <main>
-        <div className="main__banner"></div>
-        <div>pageeeeees</div>
-      </main>
-      <footer>All right is reserved.</footer>
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Footer() {
+  return <footer>All right is reserved.</footer>;
+}
+export default Footer;
