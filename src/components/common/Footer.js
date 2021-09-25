dec03719ab170367da59f1498d04055de8d27cc7
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-09-25 23:34:19 +0800

    refactor footer as styled-component

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const MyFooter = styled.footer`
+  display: flex;
+  height: 5rem;
+  background-color: var(--main-normal);
+  justify-content: center;
+  align-items: center;
+`;
+
 function Footer() {
-  return <footer>All right is reserved.</footer>;
+  return <MyFooter>All right is reserved.</MyFooter>;
 }
 export default Footer;
