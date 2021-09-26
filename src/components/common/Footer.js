7ed5fb7a5f34d8434260a098a8d8981cfacfb314
DESKTOP-COIOO24\user <mshmwr20@gmail.com>
2021-09-26 09:57:41 +0800

    refeactor footer styled-component with className

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-const MyFooter = styled.footer`
+const MyFooter = styled.footer.attrs({ className: "footer" })`
   display: flex;
   height: 5rem;
-  background-color: var(--main-normal);
+  background-color: var(--main-normal); //
   justify-content: center;
   align-items: center;
 `;
 
 function Footer() {
-  return <MyFooter>All right is reserved.</MyFooter>;
+  return <MyFooter>All right is reserved.</MyFooter>; //return 一個 styled-component
 }
 export default Footer;
