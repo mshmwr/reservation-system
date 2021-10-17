16da99c70991dec0926b943819db9c2a088affad
mshmwr <mshmwr20@gmail.com>
2021-10-17 14:22:15 +0800

    fix: set dates_date_entries overflow-y hidden, and modify maxOrderNumber according to RWD max shown order

--- src/reducer/ui/dateOrdersReducer.js
+++ src/reducer/ui/dateOrdersReducer.js
@@ -1,20 +1,24 @@
-
-import { SET_SELECTED_DTAE, SET_SHOW_DATE_ORDER_WINDOW } from "../../action/ui/dateOrderAction"
+import {
+  SET_SELECTED_DTAE,
+  SET_SHOW_DATE_ORDER_WINDOW,
+} from "../../action/ui/dateOrderAction";
 const initState = {
-    maxOrdersNumber: 3, //constant number
-    selectedDate: "",
-    showDateOrderWindow: false,
-}
+  maxOrdersNumber: 2, //constant number
+  selectedDate: "",
+  showDateOrderWindow: false,
+};
 
 const dateOrdersReducer = (state = initState, action) => {
-    switch (action.type) {
-        case SET_SELECTED_DTAE:
-            return { ...state, selectedDate: action.payload.selectedDate }
-        case SET_SHOW_DATE_ORDER_WINDOW:
-            return { ...state, showDateOrderWindow: action.payload.showDateOrderWindow }
-        default:
-            return state;
-    }
-
-}
+  switch (action.type) {
+    case SET_SELECTED_DTAE:
+      return { ...state, selectedDate: action.payload.selectedDate };
+    case SET_SHOW_DATE_ORDER_WINDOW:
+      return {
+        ...state,
+        showDateOrderWindow: action.payload.showDateOrderWindow,
+      };
+    default:
+      return state;
+  }
+};
 export default dateOrdersReducer;
